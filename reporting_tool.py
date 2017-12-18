82d8cc384e0080641f39f4ea085c39167455253b
Charles Jhong <shad0356jc@gmail.com>
2017-12-18 18:47:56 +0800

    Finish top_three_article query

--- reporting_tool.py
+++ reporting_tool.py
@@ -5,9 +5,12 @@ import psycopg2
 
 def top_three_article():
     """Returns the number of players currently registered."""
-    db = psycopg2.connect("dbname=tournament")
+    db = psycopg2.connect("dbname=news")
     c = db.cursor()
-    c.execute("select count(*) as num from players")
+    #TODO why do I have to include title in aggregate so I can select it?
+    c.execute("select articles.title, count(*) as number from log_slug, articles where log_slug.ltrim = articles.slug group by articles.slug, articles.title order by number desc limit 3")
     ans = c.fetchall()
     db.close()
-    return ans[0][0]
+    return
+
+top_three_article()
