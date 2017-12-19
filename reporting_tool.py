8da816a08b96dbbc5f9fb7114334174d1851a044
Charles Jhong <shad0356jc@gmail.com>
2017-12-19 13:01:20 +0800

    Add a new view comment

--- reporting_tool.py
+++ reporting_tool.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # implementation of log reporting tool
 # create view valid_hits as select articles.title, count(*) as hits from articles, log where log.path = '/article/'||articles.slug group by articles.title order by hits desc;
+# create view date_overview as select date_trunc('day', time) as day, cast(sum(case when status!='200 OK' then 1 else 0 end) as real) / cast(sum(case when status!='' then 1 else 0 end) as real) as err_ratio from log group by day order by day;
+
 import psycopg2
 
 def top_three_article():
