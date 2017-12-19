1cc406cca249f060e62804cc90d9301699b3da3e
Charles Jhong <shad0356jc@gmail.com>
2017-12-19 14:26:53 +0800

    PEP8 sytle applied and verified

--- reporting_tool.py
+++ reporting_tool.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 # implementation of log reporting tool
-# create view valid_hits as select articles.title, count(*) as hits from articles, log where log.path = '/article/'||articles.slug group by articles.title order by hits desc;
-# create view date_overview as select date_trunc('day', time) as day, cast(sum(case when status!='200 OK' then 1 else 0 end) as real) / cast(sum(case when status!='' then 1 else 0 end) as real) as err_ratio from log group by day order by day;
 
 import psycopg2
 import datetime
 
+
 def top_three_article():
-    #TODO DESC
+    # TODO DESC
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
-    #TODO why do I have to include title in aggregate so I can select it?
+    # TODO why do I have to include title in aggregate so I can select it?
     c.execute("select title, hits from valid_hits limit 3")
     ans = c.fetchall()
     db.close()
     for i in range(3):
-        print ("\"{}\" — {} views".format(ans[i][0],ans[i][1]))
+        print ("\"{}\" — {} views".format(ans[i][0], ans[i][1]))
     return
 
+
 def most_pop_author():
-    #TODO DESC
+    # TODO DESC
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     c.execute("select authors.name, sum(valid_hits.hits) as popularity\
@@ -29,18 +29,21 @@ def most_pop_author():
     ans = c.fetchall()
     db.close()
     for author in ans:
-        print ("{} — {} views".format(author[0],author[1]))
+        print ("{} — {} views".format(author[0], author[1]))
     return
 
+
 def err_request_day():
-    #TODO DESC
+    # TODO DESC
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
-    c.execute("select day, err_ratio from date_overview where err_ratio > 0.01")
+    c.execute("select day, err_ratio from date_overview\
+    where err_ratio > 0.01")
     ans = c.fetchall()
     db.close()
     for record in ans:
-        print ("{} — {:.1%} errors".format(record[0].strftime("%B %d, %Y"),record[1]))
+        print ("{} — {:.1%} errors".format(
+            record[0].strftime("%B %d, %Y"), record[1]))
     return
 
 
